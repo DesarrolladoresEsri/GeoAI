07a931ae5d00f4ed986d6ab320922ffbd9465e1b
Wilmar Fernando Pineda <wfpinedar@gmail.com>
2019-09-19 15:47:40 -0500

    update post for get more infor about 400 error in response

--- V2_utils/post_API_PLANET_V2.py
+++ V2_utils/post_API_PLANET_V2.py
@@ -21,7 +21,7 @@ headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 def make_ids_list(file):
     file = open(file, 'r')
     lines = file.readlines()
-    return [','.join(lines[i:i + 9]) for i in range(0, len(lines), 9)]
+    return [','.join(lines[i:i + 3]) for i in range(0, len(lines), 3)]
     
 
 def make_order(ids):
@@ -41,17 +41,17 @@ def make_order(ids):
           "email": True
        }
     }
-    print(data)
     r = requests.post(url = API_ENDPOINT, data = json.dumps(data), auth=(USER_NAME, PASSWORD), headers=headers ) 
     if r.status_code == 202:
-        order_id = r.json()['id']
-        
+        print(json.dumps(data))
+        order_id = r.json()['id']    
         print("Order id :{}".format(order_id))
         gen_ords = open('orders.log', 'a')
         gen_ords.write(order_id + '\n')
         time.sleep(60)
     else:
         print("Bad response, with status {}".format(r.status_code))
+        print("Cause By: {}".format(r.json()['field']))
 
 if __name__ == "__main__" and __package__ is None:
     start_time = time.time()
